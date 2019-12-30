e0434e401d8bfa3a70f33c02c79c2c9565a065cb
柴士童 <836275709@qq.com>
2019-12-30 20:10:00 +0100

    Update RSA_decode.py

--- RSA_decode.py
+++ RSA_decode.py
@@ -4,8 +4,11 @@ def rsa_decode(c, d, n):
     m = pow(c, d, n)
     print("Decode message int: %d"% m)
     return int2string(m)
-    
-c = int(input("Enter encrypted code:"))
-d = int(input("Enter private key d:"))
-n = int(input("Enter public key n:"))
+
+c_str = str(input("Enter encrypted code:"))
+d_str = str(input("Enter private key d:"))
+n_str = str(input("Enter public key n:"))
+c = ascii2int(c_str)
+d = ascii2int(d_str)
+n = ascii2int(n_str)
 print(rsa_decode(c,d,n))
